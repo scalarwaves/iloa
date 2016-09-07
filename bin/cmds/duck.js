25f2dd7c708f647bdaa8ce0b937c67439acad649
Andrew Prentice <drawnepicenter@gmail.com>
2016-09-07 12:13:26 -0400

    feat(duck): DuckDuckGo Instant Answers

---
+++ bin/cmds/duck.js
@@ -0,0 +1,188 @@
+'use strict';
+
+/* eslint max-len:0 */
+var themes = require('../themes');
+var tools = require('../tools');
+
+var _ = require('lodash');
+var exec = require('child_process').exec;
+var http = require('good-guy-http')();
+var noon = require('noon');
+
+var CFILE = process.env.HOME + '/.iloa.noon';
+
+exports.command = 'duck <query>';
+exports.desc = 'DuckDuckGo Instant Answers';
+exports.builder = {
+  out: {
+    alias: 'o',
+    desc: 'Write cson, json, noon, plist, yaml, xml',
+    default: '',
+    type: 'string'
+  },
+  force: {
+    alias: 'f',
+    desc: 'Force overwriting outfile',
+    default: false,
+    type: 'boolean'
+  }
+};
+exports.handler = function (argv) {
+  tools.checkConfig(CFILE);
+  var config = noon.load(CFILE);
+  var theme = themes.loadTheme(config.theme);
+  if (config.verbose) themes.label(theme, 'down', 'DuckDuckGo');
+  var dcont = [];
+  dcont.push(argv.query);
+  if (argv._.length > 1) {
+    _.each(argv._, function (value) {
+      if (value !== 'duck') dcont.push(value);
+    });
+  }
+  var words = '';
+  if (dcont.length > 1) {
+    words = dcont.join('+');
+  } else {
+    words = dcont[0];
+  }
+  var url = 'https://api.duckduckgo.com/?q=' + words + '&format=json&pretty=1&no_redirect=1&t=iloa';
+  url = encodeURI(url);
+  // console.log(url)
+  var tofile = {
+    type: 'duckduckgo',
+    source: 'https://www.duckduckgo.com/',
+    url: url
+  };
+  http({ url: url }, function (error, response) {
+    if (!error && response.statusCode === 200) {
+      var blank = {
+        DefinitionSource: '',
+        Heading: '',
+        ImageWidth: '',
+        RelatedTopics: [],
+        Entity: '',
+        meta: null,
+        Type: '',
+        Redirect: '',
+        DefinitionURL: '',
+        AbstractURL: '',
+        Definition: '',
+        AbstractSource: '',
+        Infobox: '',
+        Image: '',
+        ImageIsLogo: '',
+        Abstract: '',
+        AbstractText: '',
+        AnswerType: '',
+        ImageHeight: '',
+        Answer: '',
+        Results: []
+      };
+      // console.log(response.body)
+      var body = JSON.parse(response.body);
+      if (body === blank) {
+        console.log('DuckDuckGo found no results.');
+        process.exit(0);
+      }
+      if (body.Type === 'E' && body.Redirect != '') {
+        console.log('!bang redirect to ' + body.Redirect);
+        process.exit(0);
+      }
+      var rtype = null;
+      if (body.Type != '') {
+        if (body.Type === 'A') rtype = 'Article';
+        if (body.Type === 'C') rtype = 'Category';
+        if (body.Type === 'D') rtype = 'Disambiguation';
+        if (body.Type === 'N') rtype = 'Name';
+      } else rtype = body.Type;
+      tofile['answerType'] = body.AnswerType;
+      if (body.AnswerType === 'calc') {
+        themes.label(theme, 'right', 'Calculation');
+        console.log(tools.stripHTML(body.Answer));
+        process.exit(0);
+      }
+      if (body.AnswerType === 'phone') {
+        themes.label(theme, 'right', 'Phone number');
+        var href = body.Answer.match(/<a href="(http:\/\/[a-z0-9\.\/\?=]*) *">/i);
+        console.log(tools.stripHTML(body.Answer) + ': ' + href[1]);
+        process.exit(0);
+      }
+      if (body.AnswerType === 'unicode' || body.AnswerType === 'unicode_conversion') {
+        themes.label(theme, 'right', 'Unicode conversion');
+        console.log(body.Answer);
+        process.exit(0);
+      }
+      if (rtype != '') themes.label(theme, 'right', 'Type', rtype);
+      tofile['responseType'] = rtype;
+      var results = body.Results;
+      if (rtype === 'Article') {
+        themes.label(theme, 'right', 'Title', body.Heading);
+        themes.label(theme, 'right', 'Entity', body.Entity);
+        themes.label(theme, 'right', 'Source', body.AbstractSource);
+        themes.label(theme, 'right', 'URL', body.AbstractURL);
+        themes.label(theme, 'right', 'Text', body.AbstractText);
+        if (results != []) {
+          themes.label(theme, 'down', 'Primary Results');
+          for (var i = 0; i <= results - 1; i++) {
+            var res = results[i];
+            console.log(res.Text + '\n' + res.FirstURL);
+            tofile[['resultText' + i]] = res.Text;
+            tofile[['resultUrl' + i]] = res.FirstURL;
+          }
+        }
+        tofile['title'] = body.Heading;
+        tofile['entity'] = body.Entity;
+        tofile['abstractSource'] = body.AbstractSource;
+        tofile['abstractUrl'] = body.AbstractURL;
+        tofile['text'] = body.AbstractText;
+      }
+      if (rtype === 'Category' || rtype === 'Disambiguation') {
+        themes.label(theme, 'right', 'Title', body.Heading);
+        themes.label(theme, 'right', 'Source', body.AbstractSource);
+        themes.label(theme, 'right', 'URL', body.AbstractURL);
+        tofile['title'] = body.Heading;
+        tofile['abstractSource'] = body.AbstractSource;
+        tofile['abstractUrl'] = body.AbstractURL;
+      }
+      if (body.RelatedTopics != []) {
+        var rtArray = body.RelatedTopics;
+        var rcont = [];
+        var tcont = [];
+        for (var _i = 0; _i <= rtArray.length - 1; _i++) {
+          var hash = rtArray[_i];
+          if (hash.Result != undefined) {
+            rcont.push(hash);
+          } else if (hash.Name != undefined) {
+            tcont.push(hash);
+          }
+        }
+        if (rcont != []) {
+          themes.label(theme, 'down', 'Related');
+          for (var _i2 = 0; _i2 <= rcont.length - 1; _i2++) {
+            var rhash = rcont[_i2];
+            console.log(rhash.Text + '\n' + rhash.FirstURL);
+            tofile[['relatedText' + _i2]] = rhash.Text;
+            tofile[['relatedUrl' + _i2]] = rhash.FirstURL;
+          }
+        }
+        if (tcont != []) {
+          for (var _i3 = 0; _i3 <= tcont.length - 1; _i3++) {
+            var thash = tcont[_i3];
+            themes.label(theme, 'right', 'Topics', thash.Name);
+            tofile[['topicName' + _i3]] = thash.Name;
+            var tArray = thash.Topics;
+            for (var j = 0; j <= tArray.length - 1; j++) {
+              var tResult = tArray[j];
+              console.log(tResult.Text + '\n' + tResult.FirstURL);
+              tofile[['topicText' + _i3]] = tResult.Text;
+              tofile[['topicUrl' + _i3]] = tResult.FirstURL;
+            }
+          }
+        }
+      }
+      if (argv.o) tools.outFile(argv.o, argv.f, tofile);
+    } else {
+      throw new Error('HTTP ' + response.statusCode + ': ' + error);
+    }
+  });
+};
---
+++ src/cmds/duck.js
@@ -0,0 +1,186 @@
+/* eslint max-len:0 */
+const themes = require('../themes')
+const tools = require('../tools')
+
+const _ = require('lodash')
+const exec = require('child_process').exec
+const http = require('good-guy-http')()
+const noon = require('noon')
+
+const CFILE = `${process.env.HOME}/.iloa.noon`
+
+exports.command = 'duck <query>'
+exports.desc = 'DuckDuckGo Instant Answers'
+exports.builder = {
+  out: {
+    alias: 'o',
+    desc: 'Write cson, json, noon, plist, yaml, xml',
+    default: '',
+    type: 'string',
+  },
+  force: {
+    alias: 'f',
+    desc: 'Force overwriting outfile',
+    default: false,
+    type: 'boolean',
+  },
+}
+exports.handler = (argv) => {
+  tools.checkConfig(CFILE)
+  let config = noon.load(CFILE)
+  const theme = themes.loadTheme(config.theme)
+  if (config.verbose) themes.label(theme, 'down', 'DuckDuckGo')
+  const dcont = []
+  dcont.push(argv.query)
+  if (argv._.length > 1) {
+    _.each(argv._, (value) => {
+      if (value !== 'duck') dcont.push(value)
+    })
+  }
+  let words = ''
+  if (dcont.length > 1) {
+    words = dcont.join('+')
+  } else {
+    words = dcont[0]
+  }
+  let url = `https://api.duckduckgo.com/?q=${words}&format=json&pretty=1&no_redirect=1&t=iloa`
+  url = encodeURI(url)
+  // console.log(url)
+  const tofile = {
+    type: 'duckduckgo',
+    source: 'https://www.duckduckgo.com/',
+    url,
+  }
+  http({ url }, (error, response) => {
+    if (!error && response.statusCode === 200) {
+      const blank = {
+        DefinitionSource: '',
+        Heading: '',
+        ImageWidth: '',
+        RelatedTopics: [],
+        Entity: '',
+        meta: null,
+        Type: '',
+        Redirect: '',
+        DefinitionURL: '',
+        AbstractURL: '',
+        Definition: '',
+        AbstractSource: '',
+        Infobox: '',
+        Image: '',
+        ImageIsLogo: '',
+        Abstract: '',
+        AbstractText: '',
+        AnswerType: '',
+        ImageHeight: '',
+        Answer: '',
+        Results: []
+      }
+      // console.log(response.body)
+      const body = JSON.parse(response.body)
+      if (body === blank) {
+        console.log('DuckDuckGo found no results.')
+        process.exit(0)
+      }
+      if (body.Type === 'E' && body.Redirect != '') {
+        console.log(`!bang redirect to ${body.Redirect}`)
+        process.exit(0)
+      }
+      let rtype = null
+      if (body.Type != '') {
+        if (body.Type === 'A') rtype = 'Article'
+        if (body.Type === 'C') rtype = 'Category'
+        if (body.Type === 'D') rtype = 'Disambiguation'
+        if (body.Type === 'N') rtype = 'Name'
+      } else rtype = body.Type
+      tofile['answerType'] = body.AnswerType
+      if (body.AnswerType === 'calc') {
+        themes.label(theme, 'right', 'Calculation')
+        console.log(tools.stripHTML(body.Answer))
+        process.exit(0)
+      }
+      if (body.AnswerType === 'phone') {
+        themes.label(theme, 'right', 'Phone number')
+        const href = body.Answer.match(/<a href="(http:\/\/[a-z0-9\.\/\?=]*) *">/i)
+        console.log(`${tools.stripHTML(body.Answer)}: ${href[1]}`)
+        process.exit(0)
+      }
+      if (body.AnswerType === 'unicode' || body.AnswerType === 'unicode_conversion') {
+        themes.label(theme, 'right', 'Unicode conversion')
+        console.log(body.Answer)
+        process.exit(0)
+      }
+      if (rtype != '') themes.label(theme, 'right', 'Type', rtype)
+      tofile['responseType'] = rtype
+      const results = body.Results
+      if (rtype === 'Article') {
+        themes.label(theme, 'right', 'Title', body.Heading)
+        themes.label(theme, 'right', 'Entity', body.Entity)
+        themes.label(theme, 'right', 'Source', body.AbstractSource)
+        themes.label(theme, 'right', 'URL', body.AbstractURL)
+        themes.label(theme, 'right', 'Text', body.AbstractText)
+        if (results != []) {
+          themes.label(theme, 'down', 'Primary Results')
+          for (let i = 0; i <= results - 1; i++) {
+            const res = results[i]
+            console.log(`${res.Text}\n${res.FirstURL}`)
+            tofile[[`resultText${i}`]] = res.Text
+            tofile[[`resultUrl${i}`]] = res.FirstURL
+          }
+        }
+        tofile['title'] = body.Heading
+        tofile['entity'] = body.Entity
+        tofile['abstractSource'] = body.AbstractSource
+        tofile['abstractUrl'] = body.AbstractURL
+        tofile['text'] = body.AbstractText
+      }
+      if (rtype === 'Category' || rtype === 'Disambiguation') {
+        themes.label(theme, 'right', 'Title', body.Heading)
+        themes.label(theme, 'right', 'Source', body.AbstractSource)
+        themes.label(theme, 'right', 'URL', body.AbstractURL)
+        tofile['title'] = body.Heading
+        tofile['abstractSource'] = body.AbstractSource
+        tofile['abstractUrl'] = body.AbstractURL
+      }
+      if (body.RelatedTopics != []) {
+        const rtArray = body.RelatedTopics
+        const rcont = []
+        const tcont = []
+        for (let i = 0; i <= rtArray.length - 1; i++) {
+          const hash = rtArray[i]
+          if (hash.Result != undefined) {
+            rcont.push(hash)
+          } else if (hash.Name != undefined) {
+            tcont.push(hash)
+          }
+        }
+        if (rcont != []) {
+          themes.label(theme, 'down', 'Related')
+          for (let i = 0; i <= rcont.length - 1; i++) {
+            const rhash = rcont[i]
+            console.log(`${rhash.Text}\n${rhash.FirstURL}`)
+            tofile[[`relatedText${i}`]] = rhash.Text
+            tofile[[`relatedUrl${i}`]] = rhash.FirstURL
+          }
+        }
+        if (tcont != []) {
+          for (let i = 0; i <= tcont.length - 1; i++) {
+            const thash = tcont[i]
+            themes.label(theme, 'right', 'Topics', thash.Name)
+            tofile[[`topicName${i}`]] = thash.Name
+            const tArray = thash.Topics
+            for (let j = 0; j <= tArray.length - 1; j++) {
+              const tResult = tArray[j]
+              console.log(`${tResult.Text}\n${tResult.FirstURL}`)
+              tofile[[`topicText${i}`]] = tResult.Text
+              tofile[[`topicUrl${i}`]] = tResult.FirstURL
+            }
+          }
+        }
+      }
+      if (argv.o) tools.outFile(argv.o, argv.f, tofile)
+    } else {
+      throw new Error(`HTTP ${response.statusCode}: ${error}`)
+    }
+  })
+}
