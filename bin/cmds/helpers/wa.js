906d15f1db30da60a234803958d8fbcc87e2ec0c
Andrew Prentice <drawnepicenter@gmail.com>
2016-12-20 12:49:44 -0500

    refactor(project): minified bin

--- bin/cmds/helpers/wa.js
+++ bin/cmds/helpers/wa.js
@@ -1,155 +1,27 @@
-'use strict';
-
-var themes = require('../../themes');
-var tools = require('../../tools');
-
-var _ = require('lodash');
-var noon = require('noon');
-
-var CFILE = process.env.HOME + '/.iloa.noon';
-var config = noon.load(CFILE);
-var theme = themes.loadTheme(config.theme);
-
-/*
+'use strict';var themes=require('../../themes');var tools=require('../../tools');var _=require('lodash');var noon=require('noon');var CFILE=process.env.HOME+'/.iloa.noon';var config=noon.load(CFILE);var theme=themes.loadTheme(config.theme);/*
  * The main subpod handler
  * @param pn {number} The pod number
  * @param subpods {array} List of objects to process
  * @param tofile {object} The tofile object
  * @return {object} The tofile object
- */
-function subPod(pn, subpod, tofile) {
-  var tf = tofile;
-  for (var i = 0; i <= subpod.length - 1; i++) {
-    var sp = subpod[i];
-    var meta = sp.$;
-    tf[['pod' + pn]][['subpod' + i]] = {};
-    themes.label(theme, 'right', 'Subpod ' + i);
-    if (meta.title !== '') {
-      themes.label(theme, 'right', 'Title', meta.title);
-      tf[['pod' + pn]][['subpod' + i]].title = meta.title;
-    }
-    themes.label(theme, 'right', 'Text', tools.wrapStr(tools.arrToStr(sp.plaintext), true, true));
-    tf[['pod' + pn]][['subpod' + i]].text = tools.arrToStr(sp.plaintext);
-    themes.label(theme, 'right', 'Image', sp.img[0].$.src);
-    tf[['pod' + pn]][['subpod' + i]].image = sp.img[0].$.src;
-  }
-  return tf;
-}
-
-/*
+ */function subPod(pn,subpod,tofile){var tf=tofile;for(var i=0;i<=subpod.length-1;i++){var sp=subpod[i];var meta=sp.$;tf[['pod'+pn]][['subpod'+i]]={};themes.label(theme,'right','Subpod '+i);if(meta.title!==''){themes.label(theme,'right','Title',meta.title);tf[['pod'+pn]][['subpod'+i]].title=meta.title;}themes.label(theme,'right','Text',tools.wrapStr(tools.arrToStr(sp.plaintext),true,true));tf[['pod'+pn]][['subpod'+i]].text=tools.arrToStr(sp.plaintext);themes.label(theme,'right','Image',sp.img[0].$.src);tf[['pod'+pn]][['subpod'+i]].image=sp.img[0].$.src;}return tf;}/*
  * The main pod handler
  * Calls: subPod()
  *
  * @param pods {object} List of pods to process
  * @param tofile {object} The tofile object
  * @return {object} The tofile object
- */
-function handlePods(pods, tofile) {
-  var tf = tofile;
-  for (var i = 0; i <= pods.length - 1; i++) {
-    var p = pods[i];
-    var meta = p.$;
-    tf[['pod' + i]] = {};
-    themes.label(theme, 'right', 'Pod ' + i);
-    if (meta.title !== '') {
-      themes.label(theme, 'right', 'Title', meta.title);
-      tf[['pod' + i]].title = meta.title;
-    }
-    if (meta.scanner !== undefined) {
-      themes.label(theme, 'right', 'Scanner', meta.scanner);
-      tf[['pod' + i]].scanner = meta.scanner;
-    }
-    if (meta.numsubpods !== undefined) {
-      themes.label(theme, 'right', 'Subpods', parseInt(meta.numsubpods, 10));
-      tf[['pod' + i]].subpods = parseInt(meta.numsubpods, 10);
-    }
-    if (meta.position !== undefined) {
-      themes.label(theme, 'right', 'Position', parseInt(meta.position, 10));
-      tf[['pod' + i]].position = parseInt(meta.position, 10);
-    }
-    tf = subPod(i, p.subpod, tf);
-  }
-  return tf;
-}
-
-/*
+ */function handlePods(pods,tofile){var tf=tofile;for(var i=0;i<=pods.length-1;i++){var p=pods[i];var meta=p.$;tf[['pod'+i]]={};themes.label(theme,'right','Pod '+i);if(meta.title!==''){themes.label(theme,'right','Title',meta.title);tf[['pod'+i]].title=meta.title;}if(meta.scanner!==undefined){themes.label(theme,'right','Scanner',meta.scanner);tf[['pod'+i]].scanner=meta.scanner;}if(meta.numsubpods!==undefined){themes.label(theme,'right','Subpods',parseInt(meta.numsubpods,10));tf[['pod'+i]].subpods=parseInt(meta.numsubpods,10);}if(meta.position!==undefined){themes.label(theme,'right','Position',parseInt(meta.position,10));tf[['pod'+i]].position=parseInt(meta.position,10);}tf=subPod(i,p.subpod,tf);}return tf;}/*
  * Pod error handler.
  * Calls: handlePods()
  *
  * @param pods {object} The pods to process
  * @param tofile {object} The tofile object
  * @return {object} The tofile object
- */
-exports.numPods = function (pods, tofile) {
-  var tf = tofile;
-  if (!pods.error) {
-    tf = handlePods(pods, tf);
-  } else {
-    console.log('Pod error: ' + pods.error);
-  }
-  return tf;
-};
-
-/*
+ */exports.numPods=function(pods,tofile){var tf=tofile;if(!pods.error){tf=handlePods(pods,tf);}else{console.log('Pod error: '+pods.error);}return tf;};/*
  * Handle Wolfram|Alpha's assumptions about your query.
  *
  * @param assumptions {object} A list of assumptions to process
  * @param tofile {object} The tofile object
  * @return {object} The tofile object
- */
-exports.assume = function (assumptions, tofile) {
-  var tf = tofile;
-  var assume = assumptions[0].assumption;
-  themes.label(theme, 'right', 'Assumptions');
-
-  var _loop = function _loop(i) {
-    tf[['assumption' + i]] = {};
-    var meta = assume[i].$;
-    var v = assume[i].value;
-    tf[['assumption' + i]].type = meta.type;
-    if (meta.type === 'Clash') {
-      themes.label(theme, 'down', meta.type);
-      var one = v[0].$.desc;
-      var two = v[1].$.desc;
-      tf[['assumption' + i]].clash = 'Assuming ' + meta.word + ' is ' + one + '. Use ' + two + ' instead.';
-      console.log('Assuming ' + meta.word + ' is ' + one + '. Use ' + two + ' instead.');
-    }
-    if (meta.type === 'FormulaSolve') {
-      themes.label(theme, 'right', meta.type);
-      _.each(v, function (val) {
-        themes.label(theme, 'right', val.$.desc, val.$.input);
-        tf[['assumption' + i]][['' + val.$.desc]] = val.$.input;
-      });
-    }
-    if (meta.type === 'FormulaSelect') {
-      themes.label(theme, 'down', meta.type);
-      var _one = v[0].$.desc;
-      var _two = v[1].$.desc;
-      tf[['assumption' + i]].formulaSelect = 'Assuming ' + _one + '. Use ' + _two + ' instead.';
-      console.log('Assuming ' + _one + '. Use ' + _two + ' instead.');
-    }
-    if (meta.type === 'FormulaVariable') {
-      themes.label(theme, 'down', meta.type);
-      console.log(meta.desc + ': ' + v[0].$.desc);
-      tf[['assumption' + i]][['' + meta.desc]] = v[0].$.desc;
-    }
-    if (meta.type === 'FormulaVariableOption') {
-      themes.label(theme, 'down', meta.type);
-      var _one2 = v[0].$.desc;
-      var _two2 = v[1].$.desc;
-      tf[['assumption' + i]].formulaVariableOption = 'Assuming ' + _one2 + '. Use ' + _two2 + ' instead.';
-      console.log('Assuming ' + _one2 + '. Use ' + _two2 + ' instead.');
-    }
-    if (meta.type === 'FormulaVariableInclude') {
-      themes.label(theme, 'down', meta.type);
-      var _one3 = v[0].$.desc;
-      tf[['assumption' + i]].formulaVariableInclude = _one3;
-      console.log('Assuming ' + _one3 + '.');
-    }
-  };
-
-  for (var i = 0; i <= assume.length - 1; i++) {
-    _loop(i);
-  }
-  return tf;
-};
+ */exports.assume=function(assumptions,tofile){var tf=tofile;var assume=assumptions[0].assumption;themes.label(theme,'right','Assumptions');var _loop=function _loop(i){tf[['assumption'+i]]={};var meta=assume[i].$;var v=assume[i].value;tf[['assumption'+i]].type=meta.type;if(meta.type==='Clash'){themes.label(theme,'down',meta.type);var one=v[0].$.desc;var two=v[1].$.desc;tf[['assumption'+i]].clash='Assuming '+meta.word+' is '+one+'. Use '+two+' instead.';console.log('Assuming '+meta.word+' is '+one+'. Use '+two+' instead.');}if(meta.type==='FormulaSolve'){themes.label(theme,'right',meta.type);_.each(v,function(val){themes.label(theme,'right',val.$.desc,val.$.input);tf[['assumption'+i]][[''+val.$.desc]]=val.$.input;});}if(meta.type==='FormulaSelect'){themes.label(theme,'down',meta.type);var _one=v[0].$.desc;var _two=v[1].$.desc;tf[['assumption'+i]].formulaSelect='Assuming '+_one+'. Use '+_two+' instead.';console.log('Assuming '+_one+'. Use '+_two+' instead.');}if(meta.type==='FormulaVariable'){themes.label(theme,'down',meta.type);console.log(meta.desc+': '+v[0].$.desc);tf[['assumption'+i]][[''+meta.desc]]=v[0].$.desc;}if(meta.type==='FormulaVariableOption'){themes.label(theme,'down',meta.type);var _one2=v[0].$.desc;var _two2=v[1].$.desc;tf[['assumption'+i]].formulaVariableOption='Assuming '+_one2+'. Use '+_two2+' instead.';console.log('Assuming '+_one2+'. Use '+_two2+' instead.');}if(meta.type==='FormulaVariableInclude'){themes.label(theme,'down',meta.type);var _one3=v[0].$.desc;tf[['assumption'+i]].formulaVariableInclude=_one3;console.log('Assuming '+_one3+'.');}};for(var i=0;i<=assume.length-1;i++){_loop(i);}return tf;};
--- bin/tools.js
+++ bin/tools.js
@@ -1,207 +1,52 @@
-'use strict';
-
-/* eslint max-len: 0 */
-var chalk = require('chalk');
-var fs = require('fs-extra');
-var moment = require('moment');
-var noon = require('noon');
-var ts = require('term-size');
-var wrap = require('wrap-ansi');
-var xml2js = require('xml2js');
-
-var CFILE = process.env.HOME + '/.iloa.noon';
-
-/**
+'use strict';/* eslint max-len: 0 */var chalk=require('chalk');var fs=require('fs-extra');var moment=require('moment');var noon=require('noon');var ts=require('term-size');var wrap=require('wrap-ansi');var xml2js=require('xml2js');var CFILE=process.env.HOME+'/.iloa.noon';/**
   * The tools module provides useful repetitive tasks
   * @module Utils
-  */
-
-/**
-  * Wolfram|Alpha's API limit check
-  * @param  {Object} config The current config
-  * @return {Array} Updated config, proceed boolean, and reset boolean
-  */
-exports.limitWolf = function (config) {
-  var c = config;
-  var proceed = false;
-  var reset = false;
-  var stamp = new Date(c.wolf.date.stamp);
-  var days = moment(new Date()).diff(stamp, 'days');
-  if (days < 31) {
-    c.wolf.date.remain--;
-  } else if (days >= 31) {
-    reset = true;
-    c.wolf.date.stamp = new Date().toJSON();
-    c.wolf.date.remain = c.wolf.date.limit;
-    c.wolf.date.remain--;
-  }
-  c.wolf.date.remain <= 0 ? c.wolf.date.remain = 0 : proceed = true;
-  noon.save(CFILE, c);
-  return [c, proceed, reset];
-};
-
-/**
+  *//**
+    * Wolfram|Alpha's API limit check
+    * @param  {Object} config The current config
+    * @return {Array} Updated config, proceed boolean, and reset boolean
+    */exports.limitWolf=function(config){var c=config;var proceed=false;var reset=false;var stamp=new Date(c.wolf.date.stamp);var days=moment(new Date()).diff(stamp,'days');if(days<31){c.wolf.date.remain--;}else if(days>=31){reset=true;c.wolf.date.stamp=new Date().toJSON();c.wolf.date.remain=c.wolf.date.limit;c.wolf.date.remain--;}c.wolf.date.remain<=0?c.wolf.date.remain=0:proceed=true;noon.save(CFILE,c);return[c,proceed,reset];};/**
   * Wunderground's API limit check
   * @param  {Object} config The current config
   * @return {Array} Updated config, proceed boolean, and reset boolean
-  */
-exports.limitWunder = function (config) {
-  var c = config;
-  var dproceed = false;
-  var mproceed = false;
-  var dreset = false;
-  var mreset = false;
-  var dstamp = new Date(c.wunder.date.dstamp);
-  var mstamp = new Date(c.wunder.date.mstamp);
-  var day = moment(new Date()).diff(dstamp, 'hours');
-  var minute = moment(new Date()).diff(mstamp, 'seconds');
-  if (day < 24) {
-    c.wunder.date.dremain--;
-  } else if (day >= 24) {
-    dreset = true;
-    c.wunder.date.dstamp = new Date().toJSON();
-    c.wunder.date.dremain = c.wunder.date.dlimit;
-    c.wunder.date.dremain--;
-  }
-  if (minute < 60) {
-    c.wunder.date.mremain--;
-  } else if (minute >= 60) {
-    mreset = true;
-    c.wunder.date.mstamp = new Date().toJSON();
-    c.wunder.date.mremain = c.wunder.date.mlimit;
-    c.wunder.date.mremain--;
-  }
-  c.wunder.date.dremain <= 0 ? c.wunder.date.dremain = 0 : dproceed = true;
-  c.wunder.date.mremain <= 0 ? c.wunder.date.mremain = 0 : mproceed = true;
-  noon.save(CFILE, c);
-  return [c, dproceed, mproceed, dreset, mreset];
-};
-
-/**
+  */exports.limitWunder=function(config){var c=config;var dproceed=false;var mproceed=false;var dreset=false;var mreset=false;var dstamp=new Date(c.wunder.date.dstamp);var mstamp=new Date(c.wunder.date.mstamp);var day=moment(new Date()).diff(dstamp,'hours');var minute=moment(new Date()).diff(mstamp,'seconds');if(day<24){c.wunder.date.dremain--;}else if(day>=24){dreset=true;c.wunder.date.dstamp=new Date().toJSON();c.wunder.date.dremain=c.wunder.date.dlimit;c.wunder.date.dremain--;}if(minute<60){c.wunder.date.mremain--;}else if(minute>=60){mreset=true;c.wunder.date.mstamp=new Date().toJSON();c.wunder.date.mremain=c.wunder.date.mlimit;c.wunder.date.mremain--;}c.wunder.date.dremain<=0?c.wunder.date.dremain=0:dproceed=true;c.wunder.date.mremain<=0?c.wunder.date.mremain=0:mproceed=true;noon.save(CFILE,c);return[c,dproceed,mproceed,dreset,mreset];};/**
   * Checks if a file exists
   * @private
   * @param {string} path The filename to check.
   * @return {boolean} fileExists
-  */
-function checkOutfile(path) {
-  var fileExists = null;
-  try {
-    fs.statSync(path);
-    fileExists = true;
-  } catch (e) {
-    if (e.code === 'ENOENT') fileExists = false;
-  }
-  return fileExists;
-}
-
-/**
+  */function checkOutfile(path){var fileExists=null;try{fs.statSync(path);fileExists=true;}catch(e){if(e.code==='ENOENT')fileExists=false;}return fileExists;}/**
   * Converts string to boolean
   * @public
   * @param {string} value
   * @return {boolean} v
-  */
-exports.checkBoolean = function (value) {
-  var v = value;
-  if (v === 'true') v = true;
-  if (v === 'false') v = false;
-  return v;
-};
-
-/**
+  */exports.checkBoolean=function(value){var v=value;if(v==='true')v=true;if(v==='false')v=false;return v;};/**
  * Converts a boolean to a 0 or 1
  * @param  {boolean} value A boolean value
  * @return {integer} 0 or 1
- */
-exports.boolToBin = function (value) {
-  var r = null;
-  value ? r = 1 : r = 0;
-  return r;
-};
-
-/**
+ */exports.boolToBin=function(value){var r=null;value?r=1:r=0;return r;};/**
   * Checks if config exists. If not, prints init message and exits with error code.
   * @public
   * @param {string} file Configuration filepath
-  */
-exports.checkConfig = function (file) {
-  try {
-    fs.statSync(file);
-  } catch (e) {
-    if (e.code === 'ENOENT') throw new Error('No config found at ' + file + ', run: \'iloa config init\'');
-  }
-  return true;
-};
-
-/**
+  */exports.checkConfig=function(file){try{fs.statSync(file);}catch(e){if(e.code==='ENOENT')throw new Error('No config found at '+file+', run: \'iloa config init\'');}return true;};/**
   * Checks if object is a single string in an array
   * @public
   * @param {Object} obj Any object
   * @return {Object} Original object or extracted string
-  */
-exports.arrToStr = function (obj) {
-  var fixed = null;
-  Array.isArray(obj) && obj.length === 1 && typeof obj[0] === 'string' ? fixed = obj[0] : fixed = obj;
-  return fixed;
-};
-
-/**
+  */exports.arrToStr=function(obj){var fixed=null;Array.isArray(obj)&&obj.length===1&&typeof obj[0]==='string'?fixed=obj[0]:fixed=obj;return fixed;};/**
   * Strips HTML from a string
   * @public
   * @param  {string} string Text with HTML tags
   * @return {string} Plain text string
-  */
-exports.stripHTML = function (string) {
-  return string.replace(/(<([^>]+)>)/ig, '');
-};
-
-/**
+  */exports.stripHTML=function(string){return string.replace(/(<([^>]+)>)/ig,'');};/**
   * Wraps blocks of text
   * @param  {string} str Long string
   * @param  {boolean} hard true, soft false
   * @param  {boolean} wwrap true, column wrap false
   * @return {string} ANSI-wrapped string
-  */
-exports.wrapStr = function (str, hard, wwrap) {
-  var termsize = ts();
-  return wrap(str, termsize.columns, hard, wwrap);
-};
-
-/**
+  */exports.wrapStr=function(str,hard,wwrap){var termsize=ts();return wrap(str,termsize.columns,hard,wwrap);};/**
   * Handles data export to file. Supports cson, json, noon, plist, xml, yaml.
   * @public
   * @param {string} path The desired filepath and extension
   * @param {boolean} force Whether to force overwrite
   * @param {Object} tofile A numbered object of data points
-  */
-exports.outFile = function (path, force, tofile) {
-  var match = path.match(/\.([a-z]*)$/i);
-  var ext = match[1];
-  var builder = new xml2js.Builder();
-  if (ext === 'xml') {
-    if (checkOutfile(path)) {
-      if (force) {
-        var xml = builder.buildObject(tofile);
-        var fd = fs.openSync(path, 'w+');
-        fs.writeSync(fd, xml);
-        fs.closeSync(fd);
-        console.log(chalk.white('Overwrote ' + path + ' with data.'));
-      } else console.log(chalk.white(path + ' exists, use -f to force overwrite.'));
-    } else {
-      var _xml = builder.buildObject(tofile);
-      var _fd = fs.openSync(path, 'w+');
-      fs.writeSync(_fd, _xml);
-      fs.closeSync(_fd);
-      console.log(chalk.white('Wrote data to ' + path + '.'));
-    }
-  } else if (ext === 'cson' || ext === 'json' || ext === 'noon' || ext === 'plist' || ext === 'yml' || ext === 'yaml') {
-    if (checkOutfile(path)) {
-      if (force) {
-        noon.save(path, tofile);
-        console.log(chalk.white('Overwrote ' + path + ' with data.'));
-      } else console.log(chalk.white(path + ' exists, use -f to force overwrite.'));
-    } else {
-      noon.save(path, tofile);
-      console.log(chalk.white('Wrote data to ' + path + '.'));
-    }
-  } else if (ext !== 'xml' || ext !== 'cson' || ext !== 'json' || ext !== 'noon' || ext !== 'plist' || ext !== 'yml' || ext !== 'yaml') throw new Error('Format ' + ext + ' not supported.');
-};
+  */exports.outFile=function(path,force,tofile){var match=path.match(/\.([a-z]*)$/i);var ext=match[1];var builder=new xml2js.Builder();if(ext==='xml'){if(checkOutfile(path)){if(force){var xml=builder.buildObject(tofile);var fd=fs.openSync(path,'w+');fs.writeSync(fd,xml);fs.closeSync(fd);console.log(chalk.white('Overwrote '+path+' with data.'));}else console.log(chalk.white(path+' exists, use -f to force overwrite.'));}else{var _xml=builder.buildObject(tofile);var _fd=fs.openSync(path,'w+');fs.writeSync(_fd,_xml);fs.closeSync(_fd);console.log(chalk.white('Wrote data to '+path+'.'));}}else if(ext==='cson'||ext==='json'||ext==='noon'||ext==='plist'||ext==='yml'||ext==='yaml'){if(checkOutfile(path)){if(force){noon.save(path,tofile);console.log(chalk.white('Overwrote '+path+' with data.'));}else console.log(chalk.white(path+' exists, use -f to force overwrite.'));}else{noon.save(path,tofile);console.log(chalk.white('Wrote data to '+path+'.'));}}else if(ext!=='xml'||ext!=='cson'||ext!=='json'||ext!=='noon'||ext!=='plist'||ext!=='yml'||ext!=='yaml')throw new Error('Format '+ext+' not supported.');};
